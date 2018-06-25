419f001ea72638556552049ef59c95936403aa41
Dennis Christoffersen <dennis@fantastisk.dk>
2018-06-25 14:01:43 +0200

    refactor: added filter and load btns

--- src/denux-example/DenuxList.js
+++ src/denux-example/DenuxList.js
@@ -2,7 +2,7 @@
  * Created by @author @ddennis - ddennis.dk aka fantastisk.dk/works aka meresukker.dk on 21-06-2018.
  */
 import React, { Component } from 'react'
-import { Denux  } from "./Denux";
+import { Denux } from "./Denux";
 
 export default class DenuxList extends Component {
 
@@ -10,8 +10,6 @@ export default class DenuxList extends Component {
 		super(props)
 		this.counter = 0
 	}
-
-
 
 	render(){
 		return (
@@ -25,7 +23,6 @@ export default class DenuxList extends Component {
 								<div key={index}>
 									<h1>{item}</h1>
 									<p onClick={() =>{
-
 										context.dispatch({type:"DELETE", index:index})
 									}} style={{cursor:"pointer"}}>delete</p>
 								</div>
@@ -35,14 +32,38 @@ export default class DenuxList extends Component {
 						return (
 							<div>
 								{items}
-								<hr/>
-								<div onClick={() =>{
-									this.counter++
-									context.dispatch({type:"ADD", item:"item " + this.counter})
-								}}>
-									Add new
-								</div>
 
+								<hr/>
+
+								<div style={{display:"flex"}}>
+									<button onClick={() =>{
+										this.counter++;
+										context.dispatch({type:"ADD", item:"item " + this.counter})
+									}}>
+										Add new
+
+									</button>
+
+
+									<button className="filter" onClick={() =>{
+
+										context.dispatch({type:"SHOW_ALL"})
+									}}>
+										show all
+									</button>
+
+									<button className="filter" onClick={() =>{
+										context.dispatch({type:"SHOW_FIRST"})
+									}}>
+										show first
+									</button>
+
+									<button className="load" onClick={() =>{
+										context.dispatch({type:"START_LOAD"})
+									}}>
+										load
+									</button>
+								</div>
 							</div>
 						)
 					}
