6368a37aaed817c530524034d30afa23f0491399
Dennis Christoffersen <dennis@fantastisk.dk>
2018-06-25 16:49:01 +0200

    refactor: example of how to use reducers with async data

---
+++ src/denux-example/fetchData.js
@@ -0,0 +1,39 @@
+/**
+ * Created by @author @ddennis - ddennis.dk aka fantastisk.dk/works aka meresukker.dk on 25-06-2018.
+ */
+
+// Example of handing async data, like loading data
+
+const CUSTOM_LOAD_START = "LOAD_START";
+const CUSTOM_LOAD_COMPLETED = "LOAD_COMPLETED";
+const CUSTOM_UPDATE_DATA = "ADD_MANY";
+
+export function fetchSomeData (dispatch, user ){ {
+
+		// show loader/spinner
+		dispatch({type:CUSTOM_LOAD_START});
+
+		return fetch('https://api.github.com/users/'+user  )
+			.then(
+				response => response.json(),
+				// Do not use catch, because that will also catch
+				// any errors in the dispatch and resulting render,
+				// causing a loop of 'Unexpected batch number' errors.
+				// https://github.com/facebook/react/issues/6895
+				error => console.log('An error occurred.', error)
+
+			).then(
+				(json) =>{
+
+					// hide loader
+					dispatch({type:CUSTOM_LOAD_COMPLETED});
+
+					// update data
+					dispatch({type:CUSTOM_UPDATE_DATA, data:[json.blog , json.login, json.location]})
+				}
+			)
+	}
+
+
+}
+
