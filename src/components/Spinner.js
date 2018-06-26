6560fd6a4002f967dbf674fb4740c55bbd68edd8
Dennis Christoffersen <dennis@fantastisk.dk>
2018-06-26 15:33:50 +0200

    refactor(spinner): is now a function component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,27 +3,13 @@
  */
 import React, { Component } from 'react'
 
-export default class Spinner extends Component {
-
-	constructor(props){
-		super(props)
-	}
-
-
-	render(){
-		return (
-			<div className="spinner-container">
-				<div className="spinner">
-					<div className="bounce1"></div>
-					<div className="bounce2"></div>
-					<div className="bounce3"></div>
-				</div>
-			</div>
-
-		)
-	}
-
-}
-
-
+export const Spinner = () => (
+	<div className="spinner-container">
+		<div className="spinner">
+			<div className="bounce1"></div>
+			<div className="bounce2"></div>
+			<div className="bounce3"></div>
+		</div>
+	</div>
+)
 
